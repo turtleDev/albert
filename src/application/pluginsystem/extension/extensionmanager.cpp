a2d3817c73d442b86ecb6d901e0737e77da0cdba
Manuel Schneider <manuelschneid3r@gmail.com>
2016-01-19 14:41:59 +0100

    Make prepared query lowercase. Closes #105

--- src/application/pluginsystem/extension/extensionmanager.cpp
+++ src/application/pluginsystem/extension/extensionmanager.cpp
@@ -24,7 +24,7 @@
 /** ***************************************************************************/
 void ExtensionManager::startQuery(const QString &searchTerm) {
     // Trim spaces
-    QString trimmedTerm = searchTerm.trimmed();
+    QString trimmedTerm = searchTerm.trimmed().toLower();
 
     // Ignore empty queries
     if (trimmedTerm.isEmpty()){
